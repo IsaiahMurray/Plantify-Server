d69f6e2b3bf24ba047ab4ef468fbe5a4b54b45ff
dhelmy22 <64977520+dhelmy22@users.noreply.github.com>
2020-08-12 15:55:54 -0400

    update7

--- server/app.js
+++ server/app.js
@@ -12,8 +12,8 @@ let sequelize = require('./db');
 let user = require('./controllers/usercontroller');
 const plant = require('./controllers/plantcontroller');
 // ------------------------------------------------------
-// sequelize.sync();
-sequelize.sync({force:true});
+sequelize.sync();
+// sequelize.sync({force:true});
 app.use(express.json());
 app.use(require('./middleware/headers'));
 app.use('/plant', plant);
