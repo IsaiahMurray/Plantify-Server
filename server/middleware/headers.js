e1880139f1b915ae7f0a5f7b7e7f9b68e725547a
dhelmy22 <64977520+dhelmy22@users.noreply.github.com>
2020-08-14 14:13:54 -0400

    update3

--- server/middleware/headers.js
+++ server/middleware/headers.js
@@ -1,7 +1,7 @@
 module.exports = (req, res, next) => {
 
-    res.header('access-control-allow-origin', '*'); // * means any origin
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.header('Access-Control-Allow-Origin', '*'); // * means any origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'),
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept, Authorization');
 
     next();
