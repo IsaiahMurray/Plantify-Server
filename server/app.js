a678fd2fb6c73f72fcc408120b3a7f4c3af0cc40
dhelmy22 <64977520+dhelmy22@users.noreply.github.com>
2020-08-14 14:16:58 -0400

    update4

--- server/app.js
+++ server/app.js
@@ -14,10 +14,10 @@ const plant = require('./controllers/plantcontroller');
 // ------------------------------------------------------
 sequelize.sync();
 // sequelize.sync({force:true});
-app.use(express.json());
-app.use(require('./middleware/headers'));
 app.use('/plant', plant);
 app.use('/user', user);
+app.use(require('./middleware/headers'));
+app.use(express.json());
 
 // ---------------------------------------------------
 app.listen(process.env.PORT, function(){
